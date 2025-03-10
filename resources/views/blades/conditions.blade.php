ed00ca2c75f1d3407e677b988568ad9265e115e8
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-03-10 15:01:16 +0200

    ...

--- resources/views/blades/conditions.blade.php
+++ resources/views/blades/conditions.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Blade | Practise</title>
+  <title>Blade | Practise | Conditionals statments</title>
   <style>
     .false {
       color: red;
